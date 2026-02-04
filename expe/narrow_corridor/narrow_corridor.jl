5483cc5a83c94be1ef7cf5252e0089d540cb061c
Shuyu Zhan <zsynexus@outlook.com>
2026-02-04 14:42:19 -0500

    upload narrow corridor baseline

---
+++ expe/narrow_corridor/narrow_corridor.jl
@@ -0,0 +1,144 @@
+using Algames
+using StaticArrays
+using LinearAlgebra
+
+T = Float64
+
+# Define the dynamics of the system
+p = 2 # Number of players
+model = UnicycleGame(p=p) # game with 3 players with unicycle dynamics
+n = model.n
+m = model.m
+
+# Define the horizon of the problem
+N = 60 # N time steps
+dt = 0.1 # each step lasts 0.1 second
+probsize = ProblemSize(N,model) # Structure holding the relevant sizes of the problem
+
+# Define the objective of each player
+# We use a LQR cost
+Q = [Diagonal(ones(SVector{model.ni[i],T})) for i=1:p] # Quadratic state cost
+R = [Diagonal(ones(SVector{model.mi[i],T})) for i=1:p] # Quadratic control cost
+# Desrired state
+xf = [SVector{model.ni[1],T}([2.5, 0.1, 0, 0]),
+      SVector{model.ni[2],T}([-2.5, -0.1, 3.14159, 0]),
+      ]
+# Desired control
+uf = [zeros(SVector{model.mi[i],T}) for i=1:p]
+# Objectives of the game
+game_obj = GameObjective(Q,R,xf,uf,N,model)
+radius = 0.1*ones(p)
+μ = 5.0*ones(p)
+add_collision_cost!(game_obj, radius, μ)
+
+# Define the constraints that each player must respect
+game_con = GameConstraintValues(probsize)
+# Add collision avoidance
+radius = 0.1
+add_collision_avoidance!(game_con, radius)
+# Add control bounds
+u_max =  5*ones(SVector{m,T})
+u_min = -5*ones(SVector{m,T})
+add_control_bound!(game_con, u_max, u_min)
+# # Add state bounds for player 1
+x_max =  5*ones(SVector{n,T})
+x_min = -5*ones(SVector{n,T})
+add_state_bound!(game_con, 1, x_max, x_min)
+add_state_bound!(game_con, 2, x_max, x_min)
+# Add wall constraint
+# walls = [Wall([0.0,-0.4], [1.0,-0.4], [0.,-1.])]
+# add_wall_constraint!(game_con, walls)
+# Add circle constraint
+xc = [0., 0.]
+yc = [0.5, -0.6]
+radius = [0.3, 0.4]
+add_circle_constraint!(game_con, 1, xc, yc, radius)
+add_circle_constraint!(game_con, 2, xc, yc, radius)
+
+# Define the initial state of the system
+x0 = SVector{model.n,T}([
+    -2., 2.,
+    0.1, -0.1,
+    0.0, 3.14159,
+    0.0, 0.0,
+    ])
+
+# Define the Options of the solver
+opts = Options()
+# Define the game problem
+prob = GameProblem(N,dt,x0,model,opts,game_obj,game_con)
+
+# Solve the problem
+@time newton_solve!(prob)
+
+# Visualize the Results
+using Plots
+function plot_circles!(plt, xc::AbstractVector, yc::AbstractVector, r::AbstractVector;
+                       fillalpha::Real = 0.8,
+                       fillcolor = :black,
+                       linecolor = :black,
+                       linestyle = :solid,
+                       linewidth::Real = 2,
+                       n::Int = 181)
+    @assert length(xc) == length(yc) == length(r) "xc, yc, and r must have same length"
+    for i in eachindex(r)
+        θ = range(0, 2π; length=n)
+        x = xc[i] .+ r[i] .* cos.(θ)
+        y = yc[i] .+ r[i] .* sin.(θ)
+
+        # outline
+        plot!(plt, x, y; linecolor, linestyle, linewidth, label=nothing)
+
+        # filled disk (optional)
+        plot!(plt, [x; x[1]], [y; y[1]];
+              seriestype=:shape, fillalpha=fillalpha, fillcolor=fillcolor,
+              linecolor=:transparent, label=nothing)
+
+        # mark center
+        # scatter!(plt, [xc[i]], [yc[i]]; marker=:cross, ms=5, label=nothing, c=linecolor)
+    end
+    return plt
+end
+function plot_traj_with_constraints(prob, xc, yc, r; kwargs...)
+    traj = plot!(prob.model, prob.pdtraj.pr)
+
+    xi = [Algames.state(prob.pdtraj.pr[k])[model.pz[1][1]] for k=1:N] 
+    yi = [Algames.state(prob.pdtraj.pr[k])[model.pz[1][2]] for k=1:N]
+    ri = [0.1 for j=1:N]
+    plot_circles!(traj, xi, yi, ri; fillalpha=0.9, fillcolor=:red, linecolor=:red, linestyle=:solid
+    )
+    xi = [Algames.state(prob.pdtraj.pr[k])[model.pz[2][1]] for k=1:N] 
+    yi = [Algames.state(prob.pdtraj.pr[k])[model.pz[2][2]] for k=1:N]
+    ri = [0.1 for j=1:N]
+    plot_circles!(traj, xi, yi, ri; fillalpha=0.9, fillcolor=:blue, linecolor=:blue, linestyle=:solid)
+
+    plot_circles!(traj, xc, yc, r; kwargs...)
+    xlabel!("x"); ylabel!("y"); aspect_ratio=:equal; 
+    return traj
+end
+# traj = plot(prob.model, prob.pdtraj.pr)
+
+traj = plot_traj_with_constraints(
+    prob, xc, yc, radius;
+    fillalpha=0.9, fillcolor=:black, linecolor=:black, linestyle=:solid
+)
+# plot(prob.stats)
+savefig(traj, "traj.png")
+
+using DelimitedFiles
+
+x1 = [Algames.state(prob.pdtraj.pr[k])[model.pz[1][1]] for k=1:N] 
+y1 = [Algames.state(prob.pdtraj.pr[k])[model.pz[1][2]] for k=1:N]
+# u1_all = [get_u(prob, k) for k=1:N]
+# display(u1_all)
+u1_angular = [Algames.control(prob.pdtraj.pr[k])[model.pu[1][1]] for k=1:N]
+# display(u11)
+u1_linear = [Algames.control(prob.pdtraj.pr[k])[model.pu[1][2]] for k=1:N]
+
+x2 = [Algames.state(prob.pdtraj.pr[k])[model.pz[2][1]] for k=1:N] 
+y2 = [Algames.state(prob.pdtraj.pr[k])[model.pz[2][2]] for k=1:N]
+u2_angular = [Algames.control(prob.pdtraj.pr[k])[model.pu[2][1]] for k=1:N]
+u2_linear = [Algames.control(prob.pdtraj.pr[k])[model.pu[2][2]] for k=1:N]
+
+X = vcat(x1', y1', u1_angular', u1_linear', x2', y2', u2_angular', u2_linear')
+writedlm("Algame_narrow_corridor_traj.csv", X, ',')
